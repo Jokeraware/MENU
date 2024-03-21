d71a89d04a6726bba9520639cfec294d351a03d9
Gambizzz <anelisegambini@yahoo.fr>
2024-03-21 19:05:16 +0100

    OK

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,8 +1,4 @@
 class AdminsController < ApplicationController
-  def index
-    @admin = Admin.all
-  end
-
   def new
     @admin = Admin.new
   end
@@ -15,8 +11,34 @@ class AdminsController < ApplicationController
   end
 
   def create
-  end
+    @admin = Admin.create(admin_params)
+  
+      respond_to do |format|
+        if @admin.save
+          format.html { redirect_to admin_url(@admin), notice: "User was successfully created." }
+          format.json { render :show, status: :created, location: @admin }
+        else
+          format.html { render :new, status: :unprocessable_entity }
+          format.json { render json: @admin.errors, status: :unprocessable_entity }
+        end
+      end
+    end
+
+    def update
+      @admin = Admin.find(params[:id])
+      if @admin.update(admin_params)
+        flash[:notice] = "Your account was successfully updated."
+        redirect_to admin_path(current_admin)
+      else
+        render :edit
+      end
+    end
 
   def destroy
+  end
+
+  private
+  def admin_params
+    params.require(:admin).permit(:first_name, :last_name, :city)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,35 @@ class UsersController < ApplicationController
   def show
     @user = current_user
   end
+
+  def create
+  @user = User.create(user_params)
+
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to user_url(@user), notice: "User was successfully created." }
+        format.json { render :show, status: :created, location: @user }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "Your account was successfully updated."
+      redirect_to user_path(current_user)
+    else
+      render :edit
+    end
+  end
+
+
+  private
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :city)
+  end
 end
+
