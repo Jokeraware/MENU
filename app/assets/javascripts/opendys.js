314715aa34debcc0b50f031a953674d7b898b453
jokeraware <benedicte.tran@hotmail.com>
2024-03-21 16:06:59 +0100

    ajout de la gem turbo + opendys sur toutes les pages

--- app/assets/javascripts/opendys.js
+++ app/assets/javascripts/opendys.js
@@ -1,10 +1,22 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("turbo:load", function () {
   let toggleBtn = document.getElementById("toggle-opendys");
   let body = document.body;
   let title = document.querySelector('h1');
+  let titleform = document.querySelector('h2');
+  let opendysEnabled = localStorage.getItem("opendysActive");
 
-  toggleBtn.addEventListener("click", function() {
-    body.classList.toggle("opendys");
-    title.classList.toggle("opendys");
-  })
-})
+  // Vérifier si OpenDys est activé
+  if (opendysEnabled === "true") {
+    body.classList.add("opendys");
+    title.classList.add("opendys");
+    titleform.classList.add("opendys");
+  }
+
+  toggleBtn.addEventListener("turbo:click", function () {
+    // Inverser opendys en true
+    let CurrentlyActive = body.classList.toggle("opendys");
+
+    // Enregistrer l'état dadns un stockage local
+    localStorage.setItem("opendysActive", CurrentlyActive);
+  });
+});
