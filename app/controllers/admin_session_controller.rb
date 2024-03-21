4e5ddb0e9bb415d9769e954486dcee074bd6321a
Gambizzz <anelisegambini@yahoo.fr>
2024-03-21 12:35:39 +0100

    ADMIN OKKKKKKK

--- app/controllers/admin_session_controller.rb
+++
@@ -1,8 +0,0 @@
-class AdminSessionController < ApplicationController
-     # Action pour déconnecter l'administrateur
-     def logout
-      # Supprimer la session de l'administrateur
-      session[:admin_id] = nil
-      redirect_to root_path, notice: "Vous avez été déconnecté en tant qu'administrateur."
-    end
-end
