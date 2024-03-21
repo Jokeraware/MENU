bd2443c190f40f671e970b3aff2988d0a4428d26
yeezynico <yeezy77680@gmail.com>
2024-03-21 21:25:40 +0100

    turboOK

--- app/assets/javascript/dyslexic.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let toggleBtn = document.getElementById("toggle-opendys");
-    let body = document.body;
-  
-    toggleBtn.addEventListener("click", function() {
-      body.classList.toggle("dyslexic");
-      ("body").toggleClass("active");
-    })
-  }) 
