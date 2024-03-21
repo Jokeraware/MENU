57e43907722043cf6402cad573cd4ecba175926b
NicolasBaldo <septfoisept@outlook.fr>
2024-03-21 21:30:01 +0100

    mise en place

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -8,4 +8,16 @@ class RestaurantsController < ApplicationController
     @restaurant = Restaurant.find(params[:id])
     @restaurant_name = Faker::Restaurant.name
   end
+  def new
+    @restaurant = Restaurant.new
+  end
+
+def create
+  @restaurant = Restaurant.new(params[:id])
+  if @restaurant.save
+    redirect_to @restaurant, notice: "Le restaurant a été créé avec succès."
+  else
+    render :new
+  end
+end
 end
