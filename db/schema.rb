d8865fbf7dfdc1a47b24b91e8cf06748c73675e5
NicolasBaldo <septfoisept@outlook.fr>
2024-03-26 14:23:46 +0100

    l'admin peut gérer les résas

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_26_111447) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_26_131915) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -86,6 +86,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_26_111447) do
     t.bigint "admin_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "reservation_limit"
     t.index ["admin_id"], name: "index_restaurants_on_admin_id"
   end
 
