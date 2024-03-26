c36f71ed646610ee0b2990fd15ef471e84a966e7
NicolasBaldo <septfoisept@outlook.fr>
2024-03-26 15:07:23 +0100

    corrigé

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -4,17 +4,24 @@ class ReservationsController < ApplicationController
     @reservation = @restaurant.reservations.build
   end
   
-    def create
-      @restaurant = Restaurant.find(params[:restaurant_id])
-      @reservation = @restaurant.reservations.build(reservation_params)
-      @reservation.user = current_user
-  
-      if @reservation.save
-        redirect_to @restaurant, notice: 'Reservation was successfully created.'
-      else
-        render 'restaurants/show'
-      end
+  def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    
+    if @restaurant.reservations.count >= @restaurant.reservation_limit
+      redirect_to @restaurant, alert: 'Reservation limit reached.'
+      return
     end
+    
+    @reservation = @restaurant.reservations.build(reservation_params)
+    @reservation.user = current_user
+  
+    if @reservation.save
+      redirect_to @restaurant, notice: 'Reservation was successfully created.'
+    else
+      render 'restaurants/show'
+    end
+  end
+  
   
     def show
       @reservation = Reservation.find(params[:id])
