46b0c923073cdfdf6d129410bb4ae1f9918479db
jokeraware <benedicte.tran@hotmail.com>
2024-03-26 12:37:38 +0100

    opendys ok, btn hide pwd

--- db/schema.rb
+++ db/schema.rb
@@ -50,6 +50,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_26_085040) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
