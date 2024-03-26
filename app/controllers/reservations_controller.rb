a3bfbb88c1a960dcc5a847ec9dad748923b0e536
Gambizzz <anelisegambini@yahoo.fr>
2024-03-26 18:02:30 +0100

    Step 1

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,4 +1,6 @@
 class ReservationsController < ApplicationController
+  before_action :require_login, only: [:new, :create]
+
   def new
     @restaurant = Restaurant.find(params[:restaurant_id])
     @reservation = @restaurant.reservations.build
@@ -8,7 +10,7 @@ class ReservationsController < ApplicationController
       @restaurant = Restaurant.find(params[:restaurant_id])
       @reservation = @restaurant.reservations.build(reservation_params)
       @reservation.user = current_user
-  
+
       if @reservation.save
         redirect_to @restaurant, notice: 'Reservation was successfully created.'
       else
@@ -21,9 +23,14 @@ class ReservationsController < ApplicationController
     end
   
     private
-  
     def reservation_params
       params.require(:reservation).permit(:date, :time, :restaurant_id, :user_id)
+    end
+
+    def require_login
+      unless current_user
+        redirect_to new_user_session_path
+      end
     end
   end
   
