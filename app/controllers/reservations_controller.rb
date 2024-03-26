6f4ddb3ee64ed471a0569a3c47ec890fe47ffa3e
jokeraware <benedicte.tran@hotmail.com>
2024-03-26 16:21:16 +0100

    maj reservation

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,29 +1,37 @@
 class ReservationsController < ApplicationController
+  before_action :require_login, only: [:new, :create]
+
   def new
     @restaurant = Restaurant.find(params[:restaurant_id])
     @reservation = @restaurant.reservations.build
   end
   
-    def create
-      @restaurant = Restaurant.find(params[:restaurant_id])
-      @reservation = @restaurant.reservations.build(reservation_params)
-      @reservation.user = current_user
-  
-      if @reservation.save
-        redirect_to @restaurant, notice: 'Reservation was successfully created.'
-      else
-        render 'restaurants/show'
-      end
-    end
-  
-    def show
-      @reservation = Reservation.find(params[:id])
-    end
-  
-    private
-  
-    def reservation_params
-      params.require(:reservation).permit(:date, :time, :restaurant_id, :user_id)
+  def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @reservation = @restaurant.reservations.build(reservation_params)
+    @reservation.user = current_user
+
+    if @reservation.save
+      redirect_to @restaurant, notice: 'Reservation was successfully created.'
+    else
+      render 'restaurants/show'
     end
   end
+
+  def show
+    @reservation = Reservation.find(params[:id])
+  end
+
+  private
+
+  def require_login
+    unless current_user
+      redirect_to new_user_session_path
+    end
+  end
+
+  def reservation_params
+    params.require(:reservation).permit(:date, :time, :restaurant_id, :user_id)
+  end
+end
   
