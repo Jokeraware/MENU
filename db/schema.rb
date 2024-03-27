6e3fe2bdeef6c4def3ca7bb59f16e10e26a43412
jokeraware <benedicte.tran@hotmail.com>
2024-03-27 12:30:51 +0100

    Filtre par ville ok

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_26_150824) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_26_111447) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -70,12 +70,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_26_150824) do
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
-  create_table "information", force: :cascade do |t|
-    t.text "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "reservations", force: :cascade do |t|
     t.datetime "date"
     t.time "time"
@@ -90,9 +84,11 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_26_150824) do
   create_table "restaurants", force: :cascade do |t|
     t.string "restaurant_name"
     t.bigint "admin_id"
+    t.bigint "city_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["admin_id"], name: "index_restaurants_on_admin_id"
+    t.index ["city_id"], name: "index_restaurants_on_city_id"
   end
 
   create_table "users", force: :cascade do |t|
