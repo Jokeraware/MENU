34901636849742300742e65a955db0aaea6eea13
jokeraware <benedicte.tran@hotmail.com>
2024-03-27 16:52:02 +0100

    fix v1

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,27 +1,30 @@
 class FavoritesController < ApplicationController
-    def create
-      @favorite = current_user.favorites.new(favorite_params)
-      @favorite.save
-      redirect_to @favorite.restaurant
-    end
-
-    def destroy
-      @favorite = current_user.favorites.find_by(id: params[:id])
-      if @favorite
-        @favorite.destroy
-        flash[:notice] = 'Like destroyed successfully.'
+  def create
+    @favorite = current_user.favorites.new(favorite_params)
+      if @favorite.save
+        flash[:notice] = 'Restaurant added to favorites successfully.'
       else
-        flash[:alert] = 'Like not found.'
+        flash[:alert] = 'Failed to add restaurant to favorites.'
       end
-      redirect_back(fallback_location: root_path)
+      redirect_to @favorite.restaurant
+  end
+
+  def destroy
+    @restaurant = Restaurant.find(params[:id])
+    @favorite = current_user.favorites.find_by(restaurant_id: @restaurant.id)
+    if @favorite
+      @favorite.destroy
+      flash[:notice] = 'Restaurant removed from favorites successfully.'
+    else
+      flash[:alert] = 'Failed to remove restaurant from favorites.'
     end
+      redirect_to @restaurant
+  end
 
     
-    
+  private
 
-    private
-
-    def favorite_params
-        params.require(:favorite).permit(:restaurant_id)
-    end
+  def favorite_params
+      params.require(:favorite).permit(:restaurant_id)
+  end
 end
